a3afcd895a458e7ba2621243ebdad3d2c7cc1cae
hakoniwa0124 <hakoniwa0124@gmail.com>
2025-06-23 22:52:10 +0900

    2025年6月23日追加

---
+++ 010_2/updateTableRows.js
@@ -0,0 +1,18 @@
+/**
+ * 配列 data の内容でテーブルを更新する
+ * @param {Array<string>} data - 0〜5 件のデータ
+ */
+function updateTableRows(data) {
+  const tbody = document.querySelector('#sampleTable tbody');
+  const rows  = tbody.querySelectorAll('tr');
+
+  // ① 各行を更新
+  rows.forEach((tr, idx) => {
+    if (data.length <= idx) {
+      // データが無い行：非表示
+      tr.style.display = 'none';
+      // field.[`eva_item_name_${idx + 1}`].value = hyphen;
+      // field.[`eva_item_${idx + 1}`].value = hyphen;
+    }
+  });
+}
