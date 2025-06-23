8c7fc83aa987784905b0db4157953539712a3400
hakoniwa0124 <hakoniwa0124@gmail.com>
2025-06-23 23:08:18 +0900

    2025年6月23日追加

--- 010_2/updateTableRows.js
+++ 010_2/updateTableRows.js
@@ -7,12 +7,12 @@ function updateTableRows(data) {
   const rows  = tbody.querySelectorAll('tr');
 
   // ① 各行を更新
-  rows.forEach((tr, idx) => {
-    if (data.length <= idx) {
+  for (const [index, tr] of rows.entries()) {
+    if (data.length <= index) {
       // データが無い行：非表示
       tr.style.display = 'none';
       // field.[`eva_item_name_${idx + 1}`].value = hyphen;
       // field.[`eva_item_${idx + 1}`].value = hyphen;
     }
-  });
+  }
 }
