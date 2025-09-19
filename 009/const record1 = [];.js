2344087b5818824e6c77d79314a99ddf0b02d08c
hakoniwa0124 <hakoniwa0124@gmail.com>
2025-09-20 02:14:46 +0900

    2025年9月20日追加

---
+++ 009/const record1 = [];.js
@@ -0,0 +1,22 @@
+const record1 = [];
+const record2 = [2, 4, 6];
+const record3 = [8];
+const record4 = [];
+const record5 = [];
+
+let allEmpty = true;
+for (let i = 1; i <= 5; i++) {
+  const arr = window["record" + i]; // 文字列から変数を参照
+  if (arr.length > 0) {
+    allEmpty = false;
+    break;
+  }
+}
+console.log(allEmpty); // false
+
+
+
+const records = Array.from({ length: 100 }, (_, i) => globalThis[`record${i+1}`]);
+//   .filter(Array.isArray); // 未定義や配列以外を除外
+
+const allEmpty = records.every(a => a.length === 0);
